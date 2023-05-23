45ef0961fcbba97a2560eb357006747ba0d25482
ShaviBro <shavinda20@outlook.com>
2023-05-24 03:39:45 +0530

    Update the Demo.java
    Update the Demo.java page

--- untitled1/src/com/company/start/Demo.java
+++ untitled1/src/com/company/start/Demo.java
@@ -7,6 +7,52 @@ import java.awt.event.ActionListener;
 
 public class Demo extends JFrame {
 
-    
+    private JButton startButton;
+
+    public Demo() {
+        super("Shapes Learning App");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create and set the layout for the landing page
+        JPanel panel = new JPanel(new BorderLayout());
+        getContentPane().add(panel);
+
+        // Add a welcome message and a friendly character
+        JLabel welcomeLabel = new JLabel("Welcome to Shapes Learning!");
+        welcomeLabel.setFont(new Font("Times New Roman", Font.BOLD, 36));
+        welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(welcomeLabel,BorderLayout.NORTH);
+        JLabel characterLabel = new JLabel(new ImageIcon("E:\\untitled1\\src\\com\\company\\start\\welcome.jpg"));
+        panel.add(characterLabel, BorderLayout.CENTER);
+
+        // Add the "Start" button in the center of the pages
+        startButton = new JButton("Start Learning");
+        startButton.setFont(new Font("Times New Roman", Font.BOLD, 24));
+        startButton.setPreferredSize(new Dimension(250, 80));
+        startButton.setBackground(new Color(9, 121, 105));
+        startButton.setForeground(Color.WHITE);
+        panel.add(startButton, BorderLayout.SOUTH);
+
+        // Add an action listener to the "Start" button
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Open the landing page
+                LandingPage landingPage = new LandingPage();
+                dispose();
+            }
+        });
+
+        // Set the size and visibility of the window
+        setSize(1000, 600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+        // Remove window decorations (title bar, close button, etc.)
+
+    }
+
+    public JButton getStartButton() {
+        return startButton;
+    }
 
 }
